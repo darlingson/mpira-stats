6c08d102a1b517957fa2a46bec6c77230f95f344
darlingson <dmakuwila@gmail.com>
2024-04-29 22:14:21 +0200

    created the Game interface

---
+++ backend/src/interfaces/Game.ts
@@ -0,0 +1,14 @@
+interface Game {
+    startTime: string;
+    home: string;
+    homeGoal: number;
+    awayGoal: number;
+    away: string;
+    homeCorner: number;
+    awayCorner: number;
+    HThomeCorner: number;
+    HTawayCorner: number;
+    homeShots: number;
+    awayShots: number;
+    winner: string;
+}
