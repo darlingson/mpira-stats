bc53c0459efeed943734456c88952ee3c3db91f5
darlingson <dmakuwila@gmail.com>
2025-01-11 19:43:25 +0200

    added endpoint to return goals by a player

--- src/routes/players.ts
+++ src/routes/players.ts
@@ -10,16 +10,16 @@ player_routes.get("/", async (ctx) => {
     const from = (page - 1) * itemsPerPage;
     const to = from + itemsPerPage - 1;
 
-    const { data, error, count } = await supabase
+    const {data, error, count} = await supabase
         .from("players")
-        .select("*", { count: "exact" })
+        .select("*", {count: "exact"})
         .range(from, to);
 
     if (error) {
-        return ctx.json({ error: error.message }, 500);
+        return ctx.json({error: error.message}, 500);
     }
-    if(!count){
-        return ctx.json({ "error": "number of players is null" });
+    if (!count) {
+        return ctx.json({"error": "number of players is null"});
     }
     return ctx.json({
         players: data,
@@ -35,16 +35,58 @@ player_routes.get("/", async (ctx) => {
 player_routes.get('/:slug', async (ctx) => {
     const slug = ctx.req.param('slug');
 
-    const { data, error } = await supabase.from('players').select('*').eq('slug', slug).single();
+    const {data, error} = await supabase.from('players').select('*').eq('slug', slug).single();
 
     if (error) {
         if (error.code === 'PGRST116') {
-            return ctx.json({ error: 'Player not found' }, 404);
+            return ctx.json({error: 'Player not found'}, 404);
         }
-        return ctx.json({ error: error.message }, 500);
+        return ctx.json({error: error.message}, 500);
     }
 
-    return ctx.json({ player: data });
+    return ctx.json({player: data});
 });
 
+player_routes.get('/:player_slug/goals', async (ctx) => {
+    const slug = ctx.req.param('player_slug');
+
+    const page = parseInt(ctx.req.query("page") || "1", 10);
+    const itemsPerPage = parseInt(ctx.req.query("itemsPerPage") || "10", 10);
+
+    const from = (page - 1) * itemsPerPage;
+    const to = from + itemsPerPage - 1;
+
+    const {data, error} = await supabase.from('players').select('*').eq('slug', slug).single();
+    if (error) {
+        if (error.code === 'PGRST116') {
+            return ctx.json({error: 'Player not found'}, 404);
+        }
+        return ctx.json({error: error.message}, 500);
+    }
+
+    const {data: goals, error: goal_query_error, count} = await supabase
+        .from('goalevents')
+        .select("*", {count: "exact"})
+        .range(from, to)
+        .eq('playerid', data.playerid);
+
+    if (goal_query_error) {
+        return ctx.json({error: goal_query_error.message}, 500);
+    }
+
+    if (!count) {
+        return ctx.json({error: "No goals found for this player"});
+    }
+
+    return ctx.json({
+        player: data,
+        goals,
+        pagination: {
+            page,
+            itemsPerPage,
+            totalItems: count,
+            totalPages: Math.ceil(count / itemsPerPage),
+        },
+    });
+});
 export {player_routes}
